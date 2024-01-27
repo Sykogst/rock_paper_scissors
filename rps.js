8f0d90c6732a596ede219d08a0c54f305e7c4d91
Sam Tran <samuelttran@gmail.com>
2024-01-26 21:50:07 -0700

    Write #getPlayerChoice()

--- rps.js
+++ rps.js
@@ -1,6 +1,22 @@
 function getComputerChoice() {
     const rps = ["rock", "paper", "scissors"]
-    let compChoice = rps[Math.floor(Math.random() * rps.length)]
+    let compChoice = rps[Math.floor(Math.random() * rps.length)];
     // console.log(compChoice)
     return compChoice
+}
+
+function getPlayerChoice() {
+    while (true) {
+        let playerInput = prompt("Enter: rock, paper, or scissors: ").toLowerCase();
+
+        if (playerInput === "rock" || playerInput === "paper" || playerInput === "scissors") {
+            return playerInput;
+        } else {
+            alert("Invalid choice, please enter rock, paper, or scissors.");
+        }
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+
 }
